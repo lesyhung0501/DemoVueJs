554df41feea3d5e31679b785c27be3cc86e8eea4
lesyhung0501 <91235849+lesyhung0501@users.noreply.github.com>
2022-10-30 23:51:40 +0700

    Update rooms.js

--- src/api/rooms.js
+++ src/api/rooms.js
@@ -1,16 +1,16 @@
 import axiosAPI from ".";
 
 export const getRoomListByLoactionAPI = (locationId) => {
-  return axiosAPI.get(`/api/rooms?locationId=${locationId}`);
+  return axiosAPI.get(`/rooms?locationId=${locationId}`);
 };
 
 export const getRoomDetailAPI = (roomId) => {
-  return axiosAPI.get(`/api/rooms/${roomId}`);
+  return axiosAPI.get(`/rooms/${roomId}`);
 };
 
 export const bookingRoomAPI = (data) => {
   const { token } = JSON.parse(localStorage.getItem("userLogin"));
-  return axiosAPI.post("/api/rooms/booking", data, {
+  return axiosAPI.post("/rooms/booking", data, {
     headers: {
       token,
     },
